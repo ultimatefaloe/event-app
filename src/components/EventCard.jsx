481b8b8a3cb05fae77f966be343f8765ffbc9e0d
ultimatefaloe <ultimatefaloe@outlool.com>
2026-07-23 10:28:26 +0100

    clas7: rending array of object using map

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -1,38 +1,31 @@
 import React from "react";
 
-// interface Event{
-//   id: number,
-//   name: string,
-//   description: string,
-//   date: Date,
-//   category:  "conference" | "meetup" | "workshop" | "webinar",
-//   status: "upcoming" | "ongoing" | "completed"
-//   createdAt: Date,
-//   update: Date
-// }
+const EventCard = ({ event }) => {
+  const { name, description, date, status, category, createdAt, updatedAt } = event;
 
-const EventCard = () => {
   return (
     <div className="p-2 rounded-lg border border-gray-100">
       <div className="flex justify-between items-center mb-2">
         {/* Left */}
         <div className="text-left">
-          <h3 className="font-bold">Event Name</h3>
-          <p className="text-sm text-gray-500">Event Description</p>
-          <p className="text-xs text-gray-400">Event Date</p>
+          <h3 className="font-bold">{name}</h3>
+          <p className="text-sm text-gray-500">{description}</p>
+          <p className="text-xs text-gray-400">{date}</p>
         </div>
         {/* right */}
         <div className="flex flex-col gap-2">
           <span className="py px-3 rounded-full bg-green-400/30 border border-green-400">
-            Status
+            {status}
           </span>
           <span className="py px-3 rounded-full bg-green-400/30 border border-green-400">
-            category
+            {category}
           </span>
         </div>
       </div>
       <div className="text-right">
-        <p className="text-xs text-gray-400">Created At/ Update At</p>
+        <p className="text-xs text-gray-400">
+          {updatedAt ? `Updated: ${updatedAt}` : `Created: ${createdAt}`}
+        </p>
       </div>
     </div>
   );
