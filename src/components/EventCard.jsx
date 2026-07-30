26acf6b962809c2017e911159326a250c3fd7633
ultimatefaloe <ultimatefaloe@outlool.com>
2026-07-30 10:48:56 +0100

    class 10: event card componen updated

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -18,15 +18,15 @@ const EventCard = ({ event, onEdit, onDelete }) => {
   } = event;
 
   return (
-    <div className="p-2 rounded-lg border border-blue-400 cursor-pointer">
+    <div
+      className="p-2 rounded-lg border border-blue-400 cursor-pointer"
+      onClick={() => navigate(`/events/${id}`)}
+    >
       <div className="flex justify-between items-center mb-2">
         {/* Left */}
         <div className="text-left space-y-4">
           {/* Top alignment */}
-          <div
-            className="flex items-center justify-between gap-2"
-            onClick={() => navigate(`/events/${id}`)}
-          >
+          <div className="flex items-center justify-between gap-2">
             <h3 className="font-bold">{name}</h3>
             <div className="flex items-center gap-2">
               <span className="py px-3 rounded-full bg-green-400/30 border border-green-400">
@@ -47,11 +47,20 @@ const EventCard = ({ event, onEdit, onDelete }) => {
             </div>
             {/* Action Buttons */}
             <div className="flex flex-col gap-2 z-50">
-              <button onClick={onEdit} className="flex items-center gap-1 p-2">
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onEdit();
+                }}
+                className="flex items-center gap-1 p-2"
+              >
                 <Edit className="w-4 h-4" className="text-blue-500" />
               </button>
               <button
-                onClick={onDelete}
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onDelete();
+                }}
                 className="flex items-center gap-1 p-2"
               >
                 <Trash2 className="w-4 h-4" className="text-red-500" />
